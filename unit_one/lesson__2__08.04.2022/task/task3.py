4a9e80a3bbb1cfb394ac5539125a5fe3a1bafb69
DmitriiProkofiev <prokofev.dima@mail.ru>
2022-04-11 15:21:15 +0300

    Домашние задания

---
+++ unit_one/lesson__2__08.04.2022/task/task3.py
@@ -0,0 +1,21 @@
+##todo: Самостоятельная работа
+
+# Написать программу, которая считывает два числа и выводит их сумму, разность, частное, произведение,
+# результат целочисленного деления, результат деления с остатком, результат возведения с остатком.
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+var_sum = a + b
+var_difference = a - b
+var_division = a / b
+var_multiplication = a * b
+var_quotient = a // b
+var_remainder = a % b
+var_degree = a ** b
+print("Сумма равна:", var_sum)
+print("Разность равна:", var_difference)
+print("Частное равно:", var_division)
+print("Произведение равно:", var_multiplication)
+print("Целочисленное деление равно:", var_quotient)
+print("Деление с остатком равно:", var_remainder)
+print("Возведение с остатком равно:", var_difference)
