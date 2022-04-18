8af21f1823f6f354f25b5a44cd3bbdde9e97e292
DmitriiProkofiev <prokofev.dima@mail.ru>
2022-04-18 15:11:36 +0300

    Домашние задания lessons 4

---
+++ unit_one/lesson__4__13.04.2022/hometask/task13.py
@@ -0,0 +1,9 @@
+#todo: Дан целочисленный массив размера N из 10 элементов.
+#Преобразовать массив, увеличить каждый его элемент на единицу.
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers)
+new_numbers = []
+for num in numbers:
+    new_numbers.append(num +1)
+print(new_numbers)
